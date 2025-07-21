11e8c8338bd65ef41d4711ed5bc0a8fa92868668
Deathur <matthieu.baelen@gmail.com>
2025-07-21 13:29:46 +0200

    connexion compte

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+
     //Connexion à la BDD avec XAMPP
     $host = 'localhost';
     $dbname = 'exercicebibliotheque';
@@ -22,70 +24,60 @@
         <title>Document</title>
     </head>
     <body>
-        
+        <?php 
+            if(!isset($_SESSION['user'])){
+                    echo '
+                    <form method="POST">
+                        <label>Nom</label>
+                        <br>
+                        <input type="text" name="nom">
+                        <br>
+                        <label>Mail</label>
+                        <br>
+                        <input type="password" name="mail">
+                        <br>
+                        <br>
+                        <input type="submit" name="submitConnect" value="Se connecter">
+                    </form>
+                    <p><a href="?page=createAccount">Créer un compte</a><p>
+                    ';    
+            }
+            else {
+                echo "Bonjour, " . $_SESSION['user']['nomUtilisateurs'] . " " . $_SESSION['user']['prenomUtilisateurs'] . ". Vous êtes connectés";
+                echo '
+                <form method="POST">
+                    <input type="submit" name="deconnexion" value="Se déconnecter">
+                </form>
+                ';
+            }
+            if(isset($_POST['submitConnect'])){
+                $nom = $_POST['nom'];
+                $mail = $_POST['mail'];
+                
+                $sql = "SELECT * FROM `utilisateurs` WHERE mailUtilisateurs = '$mail' AND nomUtilisateurs = '$nom'";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute();
+                $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                if($results){
+                    $_SESSION['user'] = [
+                        "id_utilisateurs" => htmlspecialchars($results[0]["id_utilisateurs"]),
+                        "nomUtilisateurs" => htmlspecialchars($results[0]["nomUtilisateurs"]),
+                        "prenomUtilisateurs" => htmlspecialchars($results[0]["prenomUtilisateurs"]),
+                        "mailUtilisateurs" => htmlspecialchars($results[0]["mailUtilisateurs"])
+                    ];
+                    header("Location: index.php");
+                }
+                else{
+                    echo "Mot de passe incorrect";
+                }
+
+            }        
+
+
+            if(isset($_POST['deconnexion'])){
+                    session_destroy();
+                    header("Location: index.php");
+            }
+        ?>
     </body>
 </html>
-<?php
-    if(!isset($_SESSION['user'])){
-        echo '
-        <form method="POST">
-            <label>Nom</label>
-            <br>
-            <input type="text" name="nom">
-            <br>
-            <label>Mail</label>
-            <br>
-            <input type="password" name="mail">
-            <br>
-            <br>
-            <input type="submit" name="submitConnect" value="Se connecter">
-        </form>
-        <p><a href="?page=createAccount">Créer un compte</a><p>
-        ';    
-    }
-    else {
-        echo '
-        <form method="POST">
-            <input type="submit" name="deconnexion" value="Se déconnecter">
-        </form>
-        ';
-    }
-
-    if(isset($_POST['submitConnect'])){
-        $nom = $_POST['nom'];
-        $mail = $_POST['mail'];
-        
-        $sql = "SELECT * FROM `utilisateurs` WHERE mailUtilisateurs = '$mail'";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        if($results){
-            $_SESSION['user'] = [
-                "id_utilisateurs" => htmlspecialchars($results[0]["id_utilisateurs"]),
-                "nomUtilisateurs" => htmlspecialchars($results[0]["nomUtilisateurs"]),
-                "prenomUtilisateurs	" => htmlspecialchars($results[0]["prenomUtilisateurs"]),
-                "mailUtilisateurs" => htmlspecialchars($results[0]["mailUtilisateurs"]),
-            ];
-            /*
-            $id = $_GET['id'];
-            $sqlId = "SELECT * FROM users WHERE id_user = '$id'";
-            $stmtId = $pdo->prepare($sqlId);
-            $stmtId->execute();
-            $resultsId = $stmtId->fetchAll(PDO::FETCH_ASSOC);
-            */
-            var_dump($_SESSION);
-        }
-        else{
-            echo "Mot de passe incorrect";
-        }
-
-    }        
-
-
-    if(isset($_POST['deconnexion'])){
-            session_destroy();
-            header("Location: index.php");
-    }
-
-?>
