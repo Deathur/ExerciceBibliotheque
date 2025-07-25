add608a946b1a40b7147fb9d154af7556e7c9d8f
Deathur <guerre1678@gmail.com>
2025-07-25 11:28:27 +0200

    css

--- ajout.php
+++ ajout.php
@@ -18,7 +18,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Ajout bibliothèque</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <p>Ajout d'écrivains</p>
